1b4eae9dea26dc20e8b6f38cc2a39a03e22bb365
newMdd <3011878963@qq.com>
2023-01-16 01:12:34 +0800

    init

---
+++ vue_3.0/src/views/mddAbout.vue
@@ -0,0 +1,12 @@
+<template>
+  <h4>about</h4>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
---
+++ vue_3.0/src/views/mddHome.vue
@@ -0,0 +1,9 @@
+<template>
+  <h4>home</h4>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
