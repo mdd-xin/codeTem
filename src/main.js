7e687b7ada4715dcfa07c46d4c667846bb166f8e
newMdd <3011878963@qq.com>
2023-03-01 20:11:55 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,27 +1,5 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
 
-import '@/styles/index.scss'
-
-// ElementPlus
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-
-import router from './router/router.js'
-
-import { createPinia } from 'pinia'
-
-const app = createApp(App)
-
-app.use(router)
-app.use(createPinia())
-
-app.use(ElementPlus)
-
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
-
-app.mount('#app')
+createApp(App).mount('#app')
